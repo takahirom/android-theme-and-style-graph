a82340e1c42fbb6f0dff37731a25ca36e90300b2
konifar <yahpeycoy0403@gmail.com>
2019-01-06 18:06:32 +0900

    Modified README

--- scripts/init.sh
+++ scripts/init.sh
@@ -12,3 +12,9 @@ git config core.sparsecheckout true
 echo appcompat/res/ > ../.git/modules/platform_frameworks_support/info/sparse-checkout
 git read-tree -mu HEAD
 cd ../
+
+cd iosched
+git config core.sparsecheckout true
+echo mobile/src/main/res/ > ../.git/modules/iosched/info/sparse-checkout
+git read-tree -mu HEAD
+cd ../
